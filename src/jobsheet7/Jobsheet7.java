88a68491ec7ab104f9dfeb5b3068157ec5cda43c
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-08-03 18:51:21 +0700

    Coba Operator Ternary

--- src/jobsheet7/Jobsheet7.java
+++ src/jobsheet7/Jobsheet7.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package jobsheet7;
-
+import java.util.Scanner
 /**
  *
  * @author GL553VD
@@ -15,6 +15,79 @@ public class Jobsheet7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String identitas = "Christo Yosia Nazareth/XRPL5/09";
+        System.out.println("Identitas :" +identitas);
+        
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Nilai a ?");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIf..1");
+        if(a<5) System.out.println("Nilai a kurang dari 5");
+        
+        System.out.println("\nIf..2");
+        if(a==5)
+            System.out.println("Nilai a sama dengan 5");
+        
+        System.out.println("\nIf..3");
+        if(a>5){
+            System.out.println("Isi variabel a:"+a);
+            System.out.println("Nilai a lebih dari 5");
+        }
+        System.out.println("\nIf..Else..");
+        if(a<5){
+            System.out.println("Isi variabel a:"+a);
+            System.out.println("Nilai a kurang dari 5");
+        }else System.out.println("Nilai a lebih dari atau sama dengan 5");
+        
+        System.out.println("\nIf..Else IF..ELSE..1");
+        if(a<5){
+             System.out.println("Nilai a kurang dari 5");
+        
+        } else if(a==5){
+            System.out.println("Nilai a sama dengan 5");
+        } else
+            System.out.println("Nilai a lebih dari 5");
+        
+        System.out.println("\nIf..Else IF..ELSE..2");
+        if(a<2){
+             System.out.println("Nilai a kurang dari 2");
+        
+        } else if(a<4){
+            System.out.println("Nilai a kurang dari 4");
+        } else if(a<6){
+            System.out.println("Nilai a kurang dari 6");
+        } else if(a==6){
+            System.out.println("Nilai a sama dengan 6");
+        } else {
+            System.out.println("Nilai a lebih dari 6");
+        }
+        
+        System.out.println("\nNested IF");
+        if(a<7){
+            System.out.println("Nilai a kurang dari 7");
+            if(a>2)
+                System.out.println("Nilai a lebih dari 2");
+            if(a<4)
+                System.out.println("Nilai a kurang dari 4");
+        }
+        
+        System.out.println("nSWITCH..CASE");
+        switch(a) {
+            case 1: System.out.println("Nilai a =>1"); break;
+            case 2: System.out.println("Nilai a =>2"); break;
+            case 3:
+                case 4: System.out.println("Nilai a =>3 atau 4"); break;
+                case 5: System.out.println("Nilai a =>5"); break;
+                default: System.out.println("Nilai a bukan antara 1-5");
+                
+                int nilai;
+                System.out.println("\nNilai anda ?");
+                nilai = scanner.nextInt();
+                
+                String grade = (nilai < 75) ? "belum kompeten":"kompeten";
+                System.out.println(nilai + " => " + grade);
+        }
         // TODO code application logic here
     }
     
