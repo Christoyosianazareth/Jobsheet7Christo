8c7932d65728d914f93c3768eaceece68836dd11
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-08-03 18:51:54 +0700

    Initial Commit

---
+++ src/jobsheet7/SaranBahan2Resep.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+import java.util.Scanner;
+/**
+ *
+ * @author GL553VD
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args){
+        String identitas = "Christo Yosia Nazareth / XRPL5 / 09";
+        System.out.println("Identitas :"+ identitas);
+        
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+        
+        
+    }
+}
