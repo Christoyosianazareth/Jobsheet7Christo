59dbfd6f543a430fb1f5d5ab736ddda602362ca6
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-08-03 21:23:13 +0700

    Tambah Tampil Resep

--- src/jobsheet7/SaranBahan2Resep.java
+++ src/jobsheet7/SaranBahan2Resep.java
@@ -40,5 +40,26 @@ public class SaranBahan2Resep {
             int bahan2 = scanner.nextInt();
         }else
             System.out.println("Mohon maaf, pilihan tidak ditemukan," + "tidak dapat memberikan saran resep");
+        
+        int bahan2 = scanner.nextInt();
+        
+        if(bahan2 >= 1 && bahan2 <= 3)
+        {
+            if(bahan1==1)
+                switch(bahan2)
+                {
+                    case 1: System.out.println("Anda dapat membuat milk shake banana"); break;
+                    case 2: System.out.println("Anda dapat membuat pisang goreng"); break;
+                    case 3: System.out.println("Anda dapat membuat pisang rebus"); break;    
+                    }
+            else
+                switch(bahan2)
+                {
+                    case 1: System.out.println("Anda dapat membuat telur mata sapi"); break;
+                    case 2: System.out.println("Anda dapat membuat sandwich telur"); break;
+                    case 3: System.out.println("Anda dapat membuat telur rebus"); break;  
+                }
+        }else
+            System.out.println("Mohon maaf, pilihan tidak ditemukan,"+"tidak dapat memberikan saran resep");
     }
 }
